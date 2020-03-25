b3b1a0c19e2d868d7a233af1114e75d5b570a4ef
ShortArrow <bamboogeneral@live.jp>
2020-03-26 02:30:42 +0900

    make work load

---
+++ main.ps1
@@ -0,0 +1,55 @@
+#region extension check
+
+#endregion
+
+#region show recent files check
+
+#endregion
+
+#region onedrive disable check
+
+#endregion
+
+#region drive hidden check
+
+#endregion
+
+#region proxy check
+
+#endregion
+
+#region ip check
+
+#endregion
+
+#region remote check
+
+#endregion
+
+#region app check
+
+#endregion
+
+#region user check
+
+#endregion
+
+#region power management check
+
+#endregion
+
+#region network check
+
+#endregion
+
+#region os version check
+
+#endregion
+
+#region os info output
+
+#endregion
+
+#region check result output
+
+#endregion
